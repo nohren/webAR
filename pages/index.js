d7154deab373fcbc728f932354408a929290ae18
nohren <orendnelson@gmail.com>
2021-04-01 12:01:25 -0700

    added support for ar see if it deploys

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,63 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>AR playground</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Welcome to AR playground
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div  className={styles.card}>
+          <h3>Android compatibility&darr;</h3>
+            <p> 
+          * Google Play Store<br></br>
+          * Android 7.0 or newer</p>
+          </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className={styles.card}>
+          <h3>Apple iOS (iphone/ipad) combatibility &darr;</h3>
+            <p>* iPhone 6s Plus and up<br></br>
+               * iPad Pro (all models)<br></br>
+               * iPad Air (3rd and 4th gen)<br></br>
+               * iPad (5th gen or later)<br></br>
+               * iPad mini (5th gen)
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
+          </div>
+
+        <p className={styles.description}>
+        Get started with your mobile phone or tablet by finding an image below and clicking on the button
+        </p>
+ 
+        <div className={styles.ar}>
+          <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+          <script nomodule src="https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js"></script>
+          <model-viewer
+            style={{width: "300px", height: "180px"}}
+            src="/images/EarthClouds.glb"
+            ios-src="/images/EarthClouds.usdz"
+            alt="You know you want explore me"
+            shadow-intensity="1"
+            camera-controls
+            auto-rotate
+            ar
+            ar-scale
+            data-js-focus-visible
+            ar-status="not-presenting">
+          </model-viewer>
+
         </div>
+      </div>
       </main>
 
       <footer className={styles.footer}>
